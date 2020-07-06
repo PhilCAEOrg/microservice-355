f3c9309c423f94a4bc657a29412f02a8140d4969
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-07-06 08:18:50 +0000

    Version Test

---
+++ db/355_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  commedit
+-- Automatically generated sql script for the service 355, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tab.
+--
+CREATE TABLE commedit.tab (
+  col INTEGER ,
+CONSTRAINT col_PK PRIMARY KEY (col)
+ 
+);
+
+
+
